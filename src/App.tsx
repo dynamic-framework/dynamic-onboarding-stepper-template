62007095a6f9b4456d661a8d1139a5c47762485b
Johann Felipe Rojas <johannrip@gmail.com>
2024-03-04 23:09:32 -0500

    chore: add effect to init context with liquid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useDContext } from '@dynamic-framework/ui-react';
+import { useEffect } from 'react';
 import MyComponent from './components/MyComponent';
+import { SITE_LANG, VARS_CURRENCY } from './config/widgetConfig';
 
 export default function App() {
+  const { setContext } = useDContext();
+
+  useEffect(() => {
+    setContext({
+      language: SITE_LANG,
+      currency: VARS_CURRENCY,
+    });
+  }, [setContext]);
+
   return (
     <div className="border border-1 container my-8 p-8 rounded shadow-sm">
       <MyComponent />
