365979961d3475af987ca631a448b21dd872b9df
Fabián Barón <ebaron@modyo.com>
2024-03-21 16:38:07 -0500

    chore: add stepper and use var liquid for current step

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { useDContext } from '@dynamic-framework/ui-react';
+import { DStepperDesktop, useDContext } from '@dynamic-framework/ui-react';
 import { useEffect } from 'react';
-import { CONTEXT_CONFIG } from './config/widgetConfig';
+import classNames from 'classnames';
+import { CONTEXT_CONFIG, CURRENT_STEP } from './config/widgetConfig';
+import { useSteps } from './hooks/useSteps';
 
 export default function App() {
   const { setContext } = useDContext();
+  const { steps } = useSteps();
 
   useEffect(() => {
     setContext(CONTEXT_CONFIG);
   }, [setContext]);
 
   return (
-    <h1>Stepper</h1>
+    <div
+      id="onboardingStepperContainer"
+      className={classNames(
+        'd-none d-md-flex',
+        'justify-content-center align-items-center',
+        'bg-gray-200 rounded-end-4',
+      )}
+    >
+      <DStepperDesktop
+        className="custom-stepper"
+        vertical
+        options={steps}
+        currentStep={CURRENT_STEP}
+      />
+    </div>
   );
 }
